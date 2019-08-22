882af8dd1f62249be1706c55f2c4bf5679974cf1
mecsantos <santos@wni.com>
2019-08-22 13:22:08 +0900

    initial

---
+++ src/notif-client.js
@@ -0,0 +1,37 @@
+import io from 'socket.io-client';
+
+class NotifClient {
+  constructor(url, path, functions, opts = {}) {
+    let options = {
+      autoConnect: false,
+      path: path,
+      cookieHttpOnly: false,
+      pingTimeout: 60000,
+      transportOptions: {
+        polling: {
+          extraHeaders: {}
+        }
+      }
+    };
+    if(opts['licensegroup'] && opts['licensegroup'] !== '')
+      options['transportOptions']['polling']['extraHeaders']['x-wni-licensegroup'] = opts['licensegroup'];
+    if(opts['idtoken'] && opts['idtoken'] !== '')
+      options['transportOptions']['polling']['extraHeaders']['id-token'] = opts['idtoken'];
+    this.socket = io(url, options);
+    // register functions
+    ['connect', 'message', 'disconnect', 'reconnect_error', 'error'].forEach(key => {
+      const func = functions[key];
+      if(func) this.socket.on(key, func);
+    });
+  }
+  connect() {
+    this.socket.open();
+  }
+  disconnect() {
+    this.socket.close();
+  }
+  isConnected() {
+    this.socket.connected;
+  }
+}
+export { NotifClient };
